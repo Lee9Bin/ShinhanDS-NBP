5c8af54b1da4bfbf8cb073cf41f03b73f6e28dba
kky <kky9616tt@gmail.com>
2023-12-13 18:38:25 +0900

    swagger-연동

---
+++ src/main/java/com/delivery/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.delivery.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/test")
+    public String hello2(){
+        return "hello world";
+    }
+
+}
