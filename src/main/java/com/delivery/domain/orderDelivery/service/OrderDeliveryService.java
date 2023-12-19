9e09420a7d523f2e3ed7b6c7ad14721d0595369d
Lee9Bin <imagen33@daum.net>
2023-12-20 00:35:28 +0900

    [feat] 주문 생성 기능

--- src/main/java/com/delivery/domain/orderDelivery/service/OrderDeliveryService.java
+++ src/main/java/com/delivery/domain/orderDelivery/service/OrderDeliveryService.java
@@ -1,16 +1,17 @@
 package com.delivery.domain.orderDelivery.service;
 
+import com.delivery.domain.member.entity.MemberEntity;
+import com.delivery.domain.member.repository.MemberRepository;
 import com.delivery.domain.orderDelivery.dto.OrderDeliveryDto;
 import com.delivery.domain.orderDelivery.entiy.OrderDelivery;
-import com.delivery.domain.orderDelivery.entiy.Status;
 import com.delivery.domain.orderDelivery.repositry.OrderDeliveryRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+
 
 @Service
 @Transactional(readOnly = true)
@@ -18,18 +19,24 @@ import java.util.Optional;
 public class OrderDeliveryService {
 
     private final OrderDeliveryRepository orderDeliveryRepository;
-
+    private final MemberRepository memberRepository;
 
     //등록하는 거니깐 기본 트랜잭션 걸어주기
     //주문하기
     @Transactional
-    public Long save(@Valid OrderDeliveryDto orderDeliveryDto){
+    public OrderDelivery save(Long id,OrderDeliveryDto orderDeliveryDto){
         // 처음 주믄을 하면 요청수락대기 단계
-        orderDeliveryDto.setStatus(Status.WAIT);
-        orderDeliveryDto.setRequestTime(LocalDateTime.now());
-        OrderDelivery orderDelivery = OrderDelivery.toEntity(orderDeliveryDto);
-        orderDelivery.createDeliveryTime(10L);
-        OrderDelivery saveId = orderDeliveryRepository.save(orderDelivery);
-        return saveId.getId();
+        Optional<MemberEntity> targetMember = memberRepository.findById(id);
+
+        if(targetMember.isPresent()){
+            OrderDelivery orderDelivery = OrderDelivery.toEntity(orderDeliveryDto, targetMember.get());
+            orderDeliveryRepository.save(orderDelivery);
+            return OrderDelivery.toEntity(orderDeliveryDto, targetMember.get());
+        }
+        return null;
+    }
+
+    public List<OrderDelivery> findAllByMemberEntity_Id(Long id){
+        return orderDeliveryRepository.findAllByMemberEntity_Id(id);
     }
 }
