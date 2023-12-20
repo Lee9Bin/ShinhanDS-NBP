39f50cb0168bb4adf79d4142428fef28267d8e8b
Lee9Bin <imagen33@daum.net>
2023-12-20 12:47:18 +0900

    [feat]주문 기능

--- src/main/java/com/delivery/domain/orderDelivery/service/OrderDeliveryService.java
+++ src/main/java/com/delivery/domain/orderDelivery/service/OrderDeliveryService.java
@@ -4,7 +4,10 @@ import com.delivery.domain.member.entity.MemberEntity;
 import com.delivery.domain.member.repository.MemberRepository;
 import com.delivery.domain.orderDelivery.dto.OrderDeliveryDto;
 import com.delivery.domain.orderDelivery.entiy.OrderDelivery;
+import com.delivery.domain.orderDelivery.entiy.Status;
 import com.delivery.domain.orderDelivery.repositry.OrderDeliveryRepository;
+import com.delivery.domain.store.entity.StoreEntity;
+import com.delivery.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,23 +23,54 @@ public class OrderDeliveryService {
 
     private final OrderDeliveryRepository orderDeliveryRepository;
     private final MemberRepository memberRepository;
+    private final StoreRepository storeRepository;
 
     //등록하는 거니깐 기본 트랜잭션 걸어주기
-    //주문하기
-    @Transactional
-    public OrderDelivery save(Long id,OrderDeliveryDto orderDeliveryDto){
+    @Transactional //주문하기
+    public OrderDeliveryDto save(OrderDeliveryDto orderDeliveryDto, Long memberID, Long storeId){
         // 처음 주믄을 하면 요청수락대기 단계
-        Optional<MemberEntity> targetMember = memberRepository.findById(id);
+        Optional<MemberEntity> targetMember = memberRepository.findById(memberID);
+        Optional<StoreEntity> targetStore = storeRepository.findById(storeId);
 
-        if(targetMember.isPresent()){
-            OrderDelivery orderDelivery = OrderDelivery.toEntity(orderDeliveryDto, targetMember.get());
-            orderDeliveryRepository.save(orderDelivery);
-            return OrderDelivery.toEntity(orderDeliveryDto, targetMember.get());
+        if(targetMember.isPresent() && targetStore.isPresent()){
+            OrderDelivery orderDelivery = OrderDelivery.toEntity(orderDeliveryDto, targetMember.get(), targetStore.get());
+            OrderDelivery saveEntity = orderDeliveryRepository.save(orderDelivery);
+            return OrderDeliveryDto.toDto(saveEntity);
         }
         return null;
     }
 
+    //주문 목록 가져오기
     public List<OrderDelivery> findAllByMemberEntity_Id(Long id){
-        return orderDeliveryRepository.findAllByMemberEntity_Id(id);
+        Optional<MemberEntity> targetMember = memberRepository.findById(id);
+        if (targetMember.isPresent()){
+            return orderDeliveryRepository.findAllByMemberEntity_Id(id);
+        }
+
+        return null;
+    }
+
+    //주문 상세 페이지
+    public OrderDeliveryDto findByID(Long id){
+        Optional<OrderDelivery> targetOrderDelivery = orderDeliveryRepository.findById(id);
+
+        if (targetOrderDelivery.isPresent()){
+            return OrderDeliveryDto.toDto(targetOrderDelivery.get());
+        }
+
+        return null;
+    }
+
+    //주문 취소 -> 상태 업데이트
+    public OrderDeliveryDto cancel(Long id){
+        Optional<OrderDelivery> targetOrderDelivery = orderDeliveryRepository.findById(id);
+
+        if (targetOrderDelivery.isPresent()){
+            OrderDelivery findEntity = targetOrderDelivery.get();
+            findEntity.cancel(Status.CANCEL);
+            return OrderDeliveryDto.toDto(findEntity);
+        }
+
+        return null;
     }
 }
