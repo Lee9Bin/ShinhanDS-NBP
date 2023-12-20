f12aa94fb3143e19d0ee7659dc001ca3bf339776
Lee9Bin <imagen33@daum.net>
2023-12-20 14:44:57 +0900

    [feat] 주문 상품 기능

---
+++ src/main/java/com/delivery/domain/orderMenu/repository/OrderMenuRepository.java
@@ -0,0 +1,7 @@
+package com.delivery.domain.orderMenu.repository;
+
+import com.delivery.domain.orderMenu.entity.OrderMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderMenuRepository extends JpaRepository<OrderMenu, Long> {
+}
