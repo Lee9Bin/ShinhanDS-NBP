09877727834d26f082e83644a6a00f73397c18bc
dnscodnjs <yun9869@naver.com>
2023-12-20 14:10:41 +0900

    포인트 추가

--- src/main/java/com/delivery/domain/point/repository/PointRepository.java
+++ src/main/java/com/delivery/domain/point/repository/PointRepository.java
@@ -1,4 +1,9 @@
 package com.delivery.domain.point.repository;
 
-public class PointRepository {
+import com.delivery.domain.point.entity.PointEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PointRepository extends JpaRepository<PointEntity, Long> {
+
 }
