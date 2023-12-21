6cc082106120cd5adb270a49d9b69faf2774860a
dnscodnjs <yun9869@naver.com>
2023-12-21 20:48:54 +0900

    포트원 추가

--- src/main/java/com/delivery/domain/menu/repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.delivery.domain.menu.repository;
-
-import com.delivery.domain.article.entity.ArticleEntity;
-import com.delivery.domain.menu.entity.MenuEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
-
-}
