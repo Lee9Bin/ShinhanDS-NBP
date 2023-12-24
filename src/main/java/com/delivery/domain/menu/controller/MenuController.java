4d7196fd8d7efa29a560d90a10ba5936ca75f83d
kky <kky9616tt@gmail.com>
2023-12-24 23:38:54 +0900

    url 경로 감춤

--- src/main/java/com/delivery/domain/menu/controller/MenuController.java
+++ src/main/java/com/delivery/domain/menu/controller/MenuController.java
@@ -6,6 +6,7 @@ import com.delivery.domain.menu.repository.MenuRepository;
 import com.delivery.domain.menu.service.MenuService;
 import com.delivery.domain.store.entity.StoreEntity;
 import com.delivery.domain.store.repository.StoreRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -30,9 +31,10 @@ public class MenuController {
     private final MenuService menuService;
     private final MenuRepository menuRepository;
 
-    @GetMapping("/{id}/menu/new")
-    public String menuSaveForm(Model model, @PathVariable Long id) {
-        Optional<StoreEntity> targetStore = storeRepository.findById(id);
+    @GetMapping("/menu/new")
+    public String menuSaveForm(Model model, HttpSession session) {
+        Long newOwnerId = (Long) session.getAttribute("ownerId");
+        Optional<StoreEntity> targetStore = storeRepository.findByOwnerEntity_Id(newOwnerId);
 
         if (targetStore.isPresent()){
             model.addAttribute("targetStore", targetStore.get());
@@ -41,7 +43,6 @@ public class MenuController {
         return "html/owner/menu_registration";
 
     }
-
     // @PostMapping("/{id}/menu/new")
     // public String menuSave(@ModelAttribute("dummyMenuDtoList") List<MenuDto> menuDtoList,
     //                        @PathVariable Long id) {
@@ -58,14 +59,15 @@ public class MenuController {
     //     return "redirect:/store/{id}/menu/new";
     // }
 
-    @PostMapping("/{id}/menu/new")
-    public String menuSave(@PathVariable Long id, @RequestBody List<MenuDto> menuDtoList, Model model) {
+
+    @PostMapping("/menu/new")
+    public String menuSave(HttpSession session, @RequestBody List<MenuDto> menuDtoList, Model model) {
         for (MenuDto menuDto : menuDtoList) {
             // 여기서 받아온 데이터로 메뉴를 저장하거나 다른 작업을 수행할 수 있습니다.
             // 예시로 저장만 하는 코드를 추가했습니다.
             System.out.println("메뉴 리스트입니다."+menuDto.toString());
         }
-        return "redirect:/store/{id}/menu/new";
+        return "redirect:/store/menu/new";
     }
     @GetMapping("{id}")
     public String detail(@PathVariable Long id, Model model) {
