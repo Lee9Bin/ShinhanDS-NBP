f827fcef55a9ed5f62886f5240850d06deabd860
dnscodnjs <yun9869@naver.com>
2023-12-24 17:23:44 +0900

    흠

--- src/main/java/com/delivery/domain/store/repository/StoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.delivery.domain.store.repository;
-
-
-import com.delivery.domain.store.entity.StoreEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<StoreEntity, String> {
-}
