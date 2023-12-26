44c9130c4d46d6b807d7b0ca5ed43bfece496a98
Lee9Bin <imagen33@daum.net>
2023-12-26 17:54:21 +0900

    가게 메뉴 넣는중

--- src/main/java/com/delivery/domain/menu/dto/MenuDto.java
+++ src/main/java/com/delivery/domain/menu/dto/MenuDto.java
@@ -4,6 +4,7 @@ import com.delivery.domain.menu.entity.MenuEntity;
 import com.delivery.domain.store.entity.StoreEntity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,11 +13,6 @@ import lombok.*;
 @ToString
 public class MenuDto {
     private Long id;
-
-    //식당 아이디
-//    @ManyToOne
-//    @JoinColumn(name = "dummy_store_id")
-//    private DummyStoreEntity dummyStoreEntity;
 
     private String name;
 
@@ -28,6 +24,7 @@ public class MenuDto {
     private String content;
 
     private String category;
+
 
     public static MenuDto toMenuDto(MenuEntity dummyMenuEntity, StoreEntity storeEntity){
         Long storeId = (storeEntity != null) ? storeEntity.getId() : null;
@@ -50,8 +47,7 @@ public class MenuDto {
                 this.name,
                 this.price,
                 this.content,
-                this.category,
-                this.picturePath
+                this.category
         );
     }
 }
--- src/main/java/com/delivery/domain/menufile/repository/MenuFileRepository.java
+++ src/main/java/com/delivery/domain/menufile/repository/MenuFileRepository.java
@@ -1,7 +1,11 @@
 package com.delivery.domain.menufile.repository;
 
 import com.delivery.domain.menufile.entity.MenuFileEntity;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuFileRepository extends JpaRepository<MenuFileEntity,Long> {
+    Optional<MenuFileEntity> findByMenuEntity_Id(Long id);
+    List<MenuFileEntity> findAllByMenuEntity_id(Long id);
 }
